3247697db7b9fdffeffa2ef2362387fde0145f60
Louis Popi <theh2o64@gmail.com>
2016-02-13 09:11:12 +0100

    onyx: Inherit AOSParadox vendor

--- full_onyx.mk
+++ full_onyx.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device_onyx.mk)
 
+# Inherit AOSParadox Vendor
+$(call inherit-product-if-exists, vendor/aosparadox/common.mk)
+
 PRODUCT_NAME := full_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus

0fb47cf187f5a743a70adb30d9833c36f96c2084
Louis Popi <theh2o64@gmail.com>
2016-02-22 07:52:50 +0100

    onyx: Add a proper description to include/camera/CameraParametersExtra.h

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -14,6 +14,23 @@
  * limitations under the License.
  */
 
+/* Camera: Add support for manual 3A.
+ *
+ * Add manual white balance mode.
+ * user can set the specific cct to lock the white balance.Just as other
+ * white balance mode, it will lock the white balance once it's set, the
+ * only difference it that the cct value is set from app.
+ * 
+ * Add manual focus mode
+ * allow app to set the focus distance with DAC value or actuator
+ * step value. Once the value is set, the focus distance is locked
+ * unless app switch it back to automatically mode
+ * 
+ * Change-Id: I0c08ad0cea27284645e9e710c26844ca24a5c477
+ * Commit-Id: https://codeaurora.org/cgit/quic/la/platform/frameworks/av/commit/?h=LA.BF.1.1.1&id=0b8cd4917f2535c59c1fdc79bcd13188b2cd3c99
+*/
+
+
 #define CAMERA_PARAMETERS_EXTRA_C \
 const char CameraParameters::WHITE_BALANCE_MANUAL_CCT[] = "manual-cct"; \
 const char CameraParameters::FOCUS_MODE_MANUAL_POSITION[] = "manual"; \

b8cb8b0ed7550fcaa1fdd3835563f0d30f24cb72
Willi Ye <williye97@gmail.com>
2016-02-28 15:45:34 +0100

    ril: Move RIL_register_socket to ril_ex.h
    Change-Id: I2f83098f3ba5b448f381f968cf95a80532f0f0f9

--- libop/opx_ril.c
+++ libop/opx_ril.c
@@ -14,9 +14,6 @@
  * limitations under the License.
  */
 
-void RIL_register_socket(void *Init, int socketType, int argc, char **argv)  {
-}
-
 /* status_t Parcel::writeString16 */
 int _ZN7android6Parcel13writeString16EPKDsj(void *addr, unsigned int size);
 int _ZN7android6Parcel13writeString16EPKtj(void *addr, unsigned int size) {
